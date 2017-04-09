76e08929f5c63a31e948d4cda26de3e04e616f28
Alex Mourtziapis <alex.mourtziapis@gmail.com>
2017-04-09 16:54:26 +0300

    Update "Mandelbrot" page

--- pages/Mandelbrot set/Mandelbrot.glsl
+++ pages/Mandelbrot set/Mandelbrot.glsl
@@ -7,30 +7,30 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 
 #define zoom 3.0
-#define MAX_ITERATIONS 128.0
+#define MAX_ITERATIONS 128
 #define MAX_DISTANCE 2.0
 
 void main( void ) {
-	// resize view matrix to imitate a 16:9 screen
-	vec2 position = gl_FragCoord.xy * zoom / u_resolution;
+	vec2 uv = gl_FragCoord.xy / u_resolution;
+    uv *= zoom;
 
 	// Calculate starting values for Mandelbrot set for the pixel
-	vec2 p0 = vec2(-zoom/2.0 - 1.0,-zoom/2.0) + position;
+	vec2 c = vec2(-zoom/2.0 - 1.0,-zoom/2.0) + uv;
 
-	float shade = 0.0;
+	highp vec3 shade = vec3(0.0);
 
 	// Mandelbrot loop.
-	vec2 p = vec2(0.0, 0.0);
-	for (float i = 0.0; i < MAX_ITERATIONS ; i ++) {
-		// The famous Mandelbrot equation (p = p0+p^2)
-		p = p0 + vec2(p.x*p.x - p.y*p.y, 2.*p.x*p.y);
+	vec2 z = vec2(0.0);
+	for (int i = 0; i < MAX_ITERATIONS ; i++) {
+		// The famous Mandelbrot equation (z = z^2+c)
+		z = vec2(z.x*z.x - z.y*z.y, 2.*z.x*z.y) + c;
 
 		// break if point becomes greater than 2.0
-		if (distance(p.x, p.y) > MAX_DISTANCE) break;
+		if (distance(z.x, z.y) > MAX_DISTANCE) break;
 
-		shade++;
+		shade += vec3(1.0-dot(z,z)*0.125,0.5,1.0);
 	}
 
-	shade = 0.03*(shade - log2(log2(dot(p,p)))); 
-	gl_FragColor = vec4(shade, shade, shade, 1.0);
+	shade = 2e-2*(shade - log2(log2(dot(z,z)))); 
+	gl_FragColor = vec4(shade, 1.0);
 }
